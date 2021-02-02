96d21755910d2b246d3dd655d97697a61ce3de92
ivan-lytvynets <ivlitvinets@gmail.com>
2021-02-02 10:36:16 +0200

    added User model

---
+++ src/main/java/com/dev/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,20 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.UserDao;
+import com.dev.cinema.lib.Dao;
+import com.dev.cinema.model.User;
+
+import java.util.Optional;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+}
