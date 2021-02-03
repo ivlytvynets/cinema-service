9b8c35aee6728d20980445579d9b8e1f870bd7d7
ivan-lytvynets <ivlitvinets@gmail.com>
2021-02-03 15:37:51 +0200

    edited registerNewShoppingCart()

--- src/main/java/com/dev/cinema/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/ShoppingCartServiceImpl.java
@@ -9,7 +9,6 @@ import com.dev.cinema.model.ShoppingCart;
 import com.dev.cinema.model.Ticket;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.ShoppingCartService;
-import java.util.ArrayList;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -38,7 +37,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public void registerNewShoppingCart(User user) {
         ShoppingCart shoppingCart = new ShoppingCart();
         shoppingCart.setUser(user);
-        shoppingCart.setTickets(new ArrayList<>());
         shoppingCartDao.add(shoppingCart);
     }
 
