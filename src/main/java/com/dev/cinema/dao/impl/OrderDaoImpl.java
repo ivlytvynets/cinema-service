eaa131ba1b43674a8461d8388290a8bd0b340de7
ivan-lytvynets <ivlitvinets@gmail.com>
2021-02-04 09:49:43 +0200

    created classes without implementations

---
+++ src/main/java/com/dev/cinema/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,18 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.OrderDao;
+import com.dev.cinema.model.Order;
+import com.dev.cinema.model.User;
+import java.util.List;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return null;
+    }
+}
---
+++ src/main/java/com/dev/cinema/service/impl/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package com.dev.cinema.service.impl;
+
+import com.dev.cinema.model.Order;
+import com.dev.cinema.model.ShoppingCart;
+import com.dev.cinema.model.User;
+import com.dev.cinema.service.OrderService;
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return null;
+    }
+}
