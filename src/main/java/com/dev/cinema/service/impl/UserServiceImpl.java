1b13e8ba3bf6ca06887537a24b195aedf133f0a6
ivan-lytvynets <ivlitvinets@gmail.com>
2021-02-18 19:56:35 +0200

    fixed circular reference

--- src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
+++ src/main/java/com/dev/cinema/service/impl/UserServiceImpl.java
@@ -5,23 +5,18 @@ import com.dev.cinema.exception.DataProcessingException;
 import com.dev.cinema.model.User;
 import com.dev.cinema.service.UserService;
 import java.util.Optional;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
-    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserDao userDao, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserDao userDao) {
         this.userDao = userDao;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User add(User user) {
-        String hashedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(hashedPassword);
         return userDao.add(user);
     }
 
